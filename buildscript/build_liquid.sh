9567db276fc7594f77a5f5229f8c3aec3d96261f
ptichalouf <ptichalouf@gmail.com>
2013-05-23 20:29:01 +0200

    some Buildscript

---
+++ buildscript/build_liquid.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# Colorize and add text parameters
+red=$(tput setaf 1)             #  red
+grn=$(tput setaf 2)             #  green
+cya=$(tput setaf 6)             #  cyan
+txtbld=$(tput bold)             # Bold
+bldred=${txtbld}$(tput setaf 1) #  red
+bldgrn=${txtbld}$(tput setaf 2) #  green
+bldblu=${txtbld}$(tput setaf 4) #  blue
+bldcya=${txtbld}$(tput setaf 6) #  cyan
+txtrst=$(tput sgr0)             # Reset
+
+DEVICE="$1"
+SYNC="$2"
+THREADS="$3"
+CLEAN="$4"
+
+# Build Date/Version
+VERSION=`date +%Y%m%d`
+
+
+# Time of build startup
+res1=$(date +%s.%N)
+
+echo -e "${cya}Building ${bldcya}Liquid Nightly-$VERSION ${txtrst}";
+echo -e ""
+echo -e ""
+echo -e
+
+# sync with latest sources
+echo -e ""
+if [ "$SYNC" == "sync" ]
+then
+   echo -e "${bldblu}Syncing latest liquid sources ${txtrst}"
+   repo sync -j"$THREADS"
+   echo -e ""
+fi
+
+# setup environment
+if [ "$CLEAN" == "clean" ]
+then
+   echo -e "${bldblu}Cleaning up out folder ${txtrst}"
+   make clobber;
+else
+  echo -e "${bldblu}Skipping out folder cleanup ${txtrst}"
+fi
+
+
+# setup environment
+echo -e "${bldblu}Setting up build environment ${txtrst}"
+. build/envsetup.sh
+
+# lunch device
+echo -e ""
+echo -e "${bldblu}Lunching your device ${txtrst}"
+lunch "liquid_$DEVICE-userdebug";
+
+echo -e ""
+echo -e "${bldblu}Starting Liquid build for $DEVICE ${txtrst}"
+
+# start compilation
+time mka liquid "liquidsmooth_$DEVICE-userdebug" -j"$THREADS";
+echo -e ""
+
+# finished? get elapsed time
+res2=$(date +%s.%N)
+echo "${bldgrn}Total time elapsed: ${txtrst}${grn}$(echo "($res2 - $res1) / 60"|bc ) minutes ($(echo "$res2 - $res1"|bc ) seconds) ${txtrst}"
+
